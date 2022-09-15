7eb2fabe89cad5bf76ff2e710d4468d04ab0572d
Aleksey <xmyrii.aleksey@yandex.ru>
2022-09-15 18:13:19 +0700

    Example002 Hello User

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,10 @@
+﻿/*ReadLine - эта команда считывает данные
+ Переменная string(строка) - в ней хранят данные введенные пользователем
+ username - имя переменной
+ Console.WriteLine - Выводит данные в консоль
+ WriteLine - переводит курсор на новую строку
+ Write - выводит всё в одну строку*/ 
+Console.Write ("Введите ваше имя");
+string username = Console.ReadLine();
+Console.Write ("Привет, ");
+Console.Write (username);
