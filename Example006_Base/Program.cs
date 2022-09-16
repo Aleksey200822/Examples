67b4d9006764b027cc34df558dfffcb893ef6125
Aleksey <xmyrii.aleksey@yandex.ru>
2022-09-16 12:53:42 +0700

    Example006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 7;
+int b = 6;
+int c = 3;
+int d = 18;
+int e = 2;
+int max = a;
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
